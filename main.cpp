a82f2ec4ebf37ac7a1de04cd634e455f268209d5
Roman <romikchup@gmail.com>
2021-12-03 18:55:35 +0700

    fix negative numbers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 double my_pow(double number, unsigned int degree){
-    double result = number;
+    double result = 1;
     for(int i = 0; i < degree; i++){
         result*=number;
+    }
+    if(number < 0 && !degree%2){
+        return abs(result);
     }
     return result;
 }
